a05a3b45a0244aaae56173e77ab853aff4e49559
Vishwajith1412 <rgvishwajith.cse2025@citchennai.net>
2026-07-23 21:11:50 +0530

    Add files via upload

---
+++ daa exp3. kruskal.py
@@ -0,0 +1,40 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=============================================== RESTART: C:/Users/vishwajith/AppData/Local/Programs/Python/Python313/daa exp3. kruskal.py ==============================================
+=== Kruskal's MST ===
+Edge (0 - 3) Weight: 5
+Edge (2 - 4) Weight: 5
+Edge (3 - 5) Weight: 6
+Edge (0 - 1) Weight: 7
+Edge (1 - 4) Weight: 7
+Edge (4 - 6) Weight: 9
+Total MST Cost: 39
+
+=== Prim's MST ===
+Edge (0 - 3) Weight: 5
+Edge (3 - 5) Weight: 6
+Edge (0 - 1) Weight: 7
+Edge (1 - 4) Weight: 7
+Edge (4 - 2) Weight: 5
+Edge (4 - 6) Weight: 9
+Total MST Cost: 39
+>>> 
+=============================================== RESTART: C:/Users/vishwajith/AppData/Local/Programs/Python/Python313/daa exp3. kruskal.py ==============================================
+=== Kruskal's MST ===
+Edge (0 - 3) Weight: 5
+Edge (2 - 4) Weight: 5
+Edge (3 - 5) Weight: 6
+Edge (0 - 1) Weight: 7
+Edge (1 - 4) Weight: 7
+Edge (4 - 6) Weight: 9
+Total MST Cost: 39
+
+=== Prim's MST ===
+Edge (0 - 3) Weight: 5
+Edge (3 - 5) Weight: 6
+Edge (0 - 1) Weight: 7
+Edge (1 - 4) Weight: 7
+Edge (4 - 2) Weight: 5
+Edge (4 - 6) Weight: 9
+Total MST Cost: 39
